ee11077e074fcfd693ba8afb435cf28741ac7b13
IgnacioRosas172 <ignacio105garcia@gmail.com>
2023-05-30 18:59:38 -0600

    Se agregaron mas scrollview en la interfaz de rutas y camiones y se arreglo el crasheo al usar ubicacion

--- app/src/main/java/com/psp/i_rutacliente/providers/GeoProvider.kt
+++ app/src/main/java/com/psp/i_rutacliente/providers/GeoProvider.kt
@@ -3,6 +3,7 @@ package com.psp.i_rutacliente.providers
 import android.util.Log
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
@@ -13,6 +14,7 @@ import org.imperiumlabs.geofirestore.GeoQuery
 class GeoProvider {
 
     val collection = FirebaseFirestore.getInstance().collection("Locations")
+    val collectionWorking = FirebaseFirestore.getInstance().collection("LocationsWorking")
     val geoFirestore = GeoFirestore(collection)
 
 
@@ -35,6 +37,10 @@ class GeoProvider {
             Log.d("Firebase", "Error: ${exception.toString()}")
 
         }
+    }
+
+    fun getLocationWorking(idDriver: String): DocumentReference {
+        return collectionWorking.document(idDriver)
     }
 
 }
